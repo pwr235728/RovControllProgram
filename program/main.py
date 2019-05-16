13956ad40ba2f277d4e93a55ba7e5b56417b7385
Mateusz <235728@student.pwr.edu.pl>
2019-05-16 21:48:46 +0200

    bug fixes

--- program/main.py
+++ program/main.py
@@ -2,13 +2,16 @@ from RovPID import *
 from XyCtrl import *
 from ZCtrl import *
 from AHRS_sim import *
-
+from rovSim import *
 import time
+
+SampleTime = 0.01
 
 ahrs = AHRS()
 bar02 = "bar02"
 
-SampleTime = 0.01
+rov_sim = RovSim(sample_time=SampleTime)
+
 
 xyCtrl = XyCtrl(ahrs=ahrs, sampleTime=SampleTime)
 xyCtrl.heading = 90   # kierunek "patrzenia" rova
@@ -16,19 +19,38 @@ xyCtrl.direction = 0    # płyń do przodu
 xyCtrl.power = 100   # 30% macoy silnikow
 
 zCtrl = ZCtrl(ahrs=ahrs, bar02=bar02, sampleTime=SampleTime)
-zCtrl.pitch = 0
+zCtrl.pitch = 45
 zCtrl.depth = 0
 
 while True:
+    ahrs.HEADING = rov_sim.heading
+    ahrs.PITCH = rov_sim.pitch
+    ahrs.PITCH_SPEED = rov_sim.pitch_speed
+    ahrs.YAW_SPEED = rov_sim.heading_speed
+
     xyCtrl.update()
     zCtrl.update()
 
+    rov_sim.thrA = xyCtrl.thrusterA
+    rov_sim.thrB = xyCtrl.thrusterB
+    rov_sim.thrC = xyCtrl.thrusterC
+    rov_sim.thrD = xyCtrl.thrusterD
 
+    rov_sim.thrZL = zCtrl.thruster_ZL
+    rov_sim.thrZR = zCtrl.thruster_ZR
+    rov_sim.thrZB = zCtrl.thruster_ZB
+
+    rov_sim.update()
+
+    print("", "{0:07.2f}".format(ahrs.HEADING), "; ", "{0:07.2f}".format(ahrs.YAW_SPEED),
+          " ;", "{0:07.2f}".format(ahrs.PITCH), "; ", "{0:07.2f}".format(ahrs.PITCH_SPEED))
+
+    time.sleep(SampleTime)
+"""
     rot = -xyCtrl.thrusterA + xyCtrl.thrusterB - xyCtrl.thrusterC + xyCtrl.thrusterD
     pow = math.fabs(xyCtrl.thrusterA) + math.fabs(xyCtrl.thrusterB) + math.fabs(xyCtrl.thrusterC) + math.fabs(xyCtrl.thrusterD)
     print("", "{0:07.2f}".format(rot),"; ",  "{0:07.2f}".format(pow), " ;", "{0:07.2f}".format(ahrs.HEADING), "; ", "{0:07.2f}".format(ahrs.YAW_SPEED))
 
     ahrs.YAW_SPEED = ahrs.YAW_SPEED + rot*SampleTime
     ahrs.HEADING = ahrs.HEADING + ahrs.YAW_SPEED*0.01
-
-    time.sleep(SampleTime)
+"""
--- program/rovSim.py
+++ program/rovSim.py
@@ -1,2 +1,36 @@
 class RovSim:
-    pass
+
+    P = 0.95
+    Q = 1.0 - P
+
+    def __init__(self, sample_time):
+        self.sample_time = sample_time
+
+        self.thrA = 0.0
+        self.thrB = 0.0
+        self.thrC = 0.0
+        self.thrD = 0.0
+        self.thrZL = 0.0
+        self.thrZR = 0.0
+        self.thrZB = 0.0
+
+        self.heading = 0.0
+        self.heading_speed = 0.0
+        self.pitch = 0.0
+        self.pitch_speed = 0.0
+
+        self.move_speed = 0.0
+
+
+    def update(self):
+        rot = self.thrB + self.thrD - self.thrA - self.thrC
+        tr = self.thrB + self.thrA - self.thrD - self.thrC
+        pitch = -self.thrZB + self.thrZL + self.thrZR
+
+        self.heading_speed = self.heading_speed * self.P + rot*self.Q
+        self.heading = self.heading + self.heading_speed*self.sample_time
+
+        self.pitch_speed= self.pitch_speed*self.P + pitch*self.Q
+        self.pitch = self.pitch + self.pitch_speed * self.sample_time
+
+        self.move_speed = self.move_speed*self.P + tr*self.Q
